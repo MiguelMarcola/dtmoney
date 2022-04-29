6680e5e136291faa66aeaea4a10f759900c8cb49
MiguelMarcola <miguelmarcola@gmail.com>
2022-04-29 17:50:21 -0300

    refatoring

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,7 +8,7 @@ export const GlobalStyle = createGlobalStyle`
         --blue: #5429cc;
         --blue-light: #6933ff;
         --text-title: #363f5f;
-        --text-body: #969cb3;
+        --text-body: #000;
         --shape: #ffffff;
     }
 
